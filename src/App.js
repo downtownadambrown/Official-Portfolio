d7067bafe9e5aaa4cbadcd0e46e15f808b2208c6
Adam Brown <downtownadambrown@gmail.com>
2020-03-07 13:59:02 -0500

    refactors into functional component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { tiles } from './util/tiles';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+const App = () => {
+    const renderHeader = () => (
+        <header className="header">
+            <h3>
+                Adam Brown
+            </h3>
+            <p>Javascript Developer</p>
         </header>
-      </div>
     );
-  }
-}
+
+    const generateKey = (tile, index) => `${tile.label.split(" ").join("-")}-${index}`;
+
+    const renderTileRow = (tiles) => {
+        return (
+            <div className="row">
+                {tiles.map((tile, index) => (
+                    <div
+                        className={tile.className}
+                        key={generateKey(tile, index)}
+                    >
+                        <span>{tile.label}</span>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    const rowKeys = ["row1", "row2", "row3"];
+
+    return (
+        <div className="App">
+            {renderHeader()}
+            <main className="main-container container p-3">
+                {rowKeys.map(row => renderTileRow(tiles[row]))}
+            </main>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/util/tiles.js
@@ -0,0 +1,75 @@
+export const tiles = {
+    row1: [
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 1,
+            className: "col tile darkgreen",
+        },
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 1,
+            className: "col tile green",
+        },
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 1,
+            className: "col tile yellow",
+
+        },
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 1,
+            className: "col tile red",
+        },
+    ],
+    row2: [
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 2,
+            className: "col tile lightblue",
+        },
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 2,
+            className: "col-6 tile blue",
+        },
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 2,
+            className: "col tile lightblue",
+        },
+    ],
+    row3: [
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 3,
+            className: "col tile red",
+        },
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 3,
+            className: "col tile yellow",
+        },
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 3,
+            className: "col tile green",
+        },
+        {
+            label: "Adams App 1",
+            github: "http://www.github.com/downtownadambrown",
+            row: 3,
+            className: "col tile darkgreen",
+        },
+    ],
+};
