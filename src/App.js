dc257ebd10cae0e0103167a8ebd46687f7d0689a
Adam Brown <downtownadambrown@gmail.com>
2020-03-28 17:48:42 -0400

    implements react-spring for initial load page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,80 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
+
+import { useSpring, useTransition, animated } from 'react-spring';
+
 import { FaTeamspeak } from "react-icons/fa";
 import { DiJsBadge } from "react-icons/di";
 import { GiF1Car } from "react-icons/gi";
 import { FaRegLightbulb } from "react-icons/fa";
 
-const Home = (props) => {
+const Home = () => {
+    const ref = useRef([]);
+    const [items, set] = useState([]);
+    const tiles = [
+        {
+            icon: <FaTeamspeak className="m-3" size="50px" />,
+            label: "Gamer",
+            delay: 800,
+        },{
+            icon: <DiJsBadge className="m-3" size="50px" />,
+            label: "Coder",
+            delay: 1300,
+        },{
+            icon: <GiF1Car className="m-3" size="50px" />,
+            label: "Engineer",
+            delay: 1800,
+        },{
+            icon: <FaRegLightbulb className="m-3" size="50px" />,
+            label: "Mentor",
+            delay: 2300,
+        }
+    ];
+
+    const animatedProps = {
+        opacity: 1,
+        from: { opacity: 0 },
+    };
+
+    const transitions = useTransition(items , null, {
+        from: { opacity: 0, height: 0, innerHeight: 0, transform: 'perspective(600px) rotateX(0deg)' },
+        enter: [
+            { opacity: 1, height: 40, innerHeight: 40 },
+            { transform: 'perspective(600px) rotateX(0deg)' },
+        ],
+        leave: [{ color: '#c23369' }, { innerHeight: 0 }, { opacity: 0, height: 0 }],
+    });
+
+    const reset = useCallback(() => {
+        ref.current.map(clearTimeout);
+        ref.current = [];
+        set([]);
+        ref.current.push(setTimeout(() => set(['Hello.']), 250));
+        ref.current.push(setTimeout(() => set(['Hello.', 'I\'m Adam Brown.']), 750));
+    }, []);
+
+    useEffect(() => void reset(), []);
+
     return (
         <div className="home d-flex flex-column align-items-center justify-content-center">
             <div className="d-flex flex-md-column flex-sm-row align-items-center">
-                <h3 className="headline">Hello.</h3>
-                <h3 className="headline">I am Adam Brown.</h3>
+                {transitions.map(({ item, props: { innerHeight, ...rest }, key }) => (
+                    <animated.div className="transitions-item" key={key} style={rest} onClick={reset}>
+                        <animated.div style={{ overflow: 'hidden', height: innerHeight }} className="introTitle">{item}</animated.div>
+                    </animated.div>
+                ))}
             </div>
             <div className="fluid-container row tile-container">
-                <div className="col-md-6 col-sm-12">
-                    <div className="buttonTile d-flex flex-md-row-reverse align-items-center">
-                        <FaTeamspeak className="m-3" size="50px" />
-                        <span>Gamer</span>
+                {tiles.map((tile, index) => (
+                    <div className="col-md-6 col-sm-12" key={`${tile.label}-${tile.delay}`}>
+                        <div className={`buttonTile d-flex align-items-center ${!(index % 2) ? "flex-md-row-reverse" : ""}`}>
+                            <animated.div style={useSpring({ ...animatedProps, delay: tile.delay })}>
+                                {tile.icon}
+                                <span>{tile.label}</span>
+                            </animated.div>
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-6 col-sm-12">
-                    <div className="buttonTile d-flex align-items-center">
-                        <DiJsBadge className="m-3" size="50px" />
-                        <span>Coder</span>
-                    </div>
-                </div>
-                <div className="col-md-6 col-sm-12">
-                    <div className="buttonTile d-flex flex-md-row-reverse align-items-center">
-                        <GiF1Car className="m-3" size="50px" />
-                        <span>Engineer</span>
-                    </div>
-                </div>
-                <div className="col-md-6 col-sm-12">
-                    <div className="buttonTile d-flex align-items-center">
-                        <FaRegLightbulb className="m-3" size="50px" />
-                        <span>Mentor</span>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     );
