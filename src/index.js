bc743057a3320e2134a97b31eeb6c3c52f8f97de
Adam Brown <downtownadambrown@gmail.com>
2020-04-13 09:37:48 -0400

    swaps font

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './font/FiraSans-Black.ttf';
+import './font/BalooTamma2-Regular.ttf';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
