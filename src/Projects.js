4f6c4400a94a440dd2ab1945beb900f8bc2c560f
Adam Brown <Adam.Brown2@coxautoinc.com>
2020-07-04 16:52:03 -0400

    builds out projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -8,6 +8,9 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import adam from './static/adam.jpg';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles({
     root: {
@@ -72,8 +75,27 @@ const ProjectCard = ({
     )
 };
 
-const Projects = (props) => {
+const TabPanel = (props) => {
+    const { children, value, index, ...other } = props;
 
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`scrollable-auto-tabpanel-${index}`}
+            aria-labelledby={`scrollable-auto-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+};
+
+const AppProjects = () => {
     const footerChildren = [{
         label: 'label test',
         props: {
@@ -82,14 +104,52 @@ const Projects = (props) => {
         },
         icon: null,
     }];
+    
+    return (
+        <ProjectCard
+            footerChildren={footerChildren}
+            title="Project 1 Title"
+            description="project 1 description here"
+        />
+    );
+};
+
+const Projects = (props) => {
+
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className="d-flex flex-column">
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                centered
+            >
+                <Tab label="Apps" />
+                <Tab label="UI Components" />
+                <Tab label="Other Projects" />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+                <AppProjects />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item 2
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Item 3
+            </TabPanel>
+        </div>
+    );
 
     return (
         <div>
-            <ProjectCard
-                footerChildren={footerChildren}
-                title="Project 1 Title"
-                description="project 1 description here"
-            />
+
         </div>
     );
 };
