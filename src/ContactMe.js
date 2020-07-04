1d2d77b4fb66e8b93386ff21c739ae44b826a685
Adam Brown <Adam.Brown2@coxautoinc.com>
2020-07-04 11:38:35 -0400

    eslint

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -7,7 +7,6 @@ import FormControl from '@material-ui/core/FormControl';
 import { makeStyles } from '@material-ui/core/styles';
 import MaskedInput from 'react-text-mask';
 import Input from '@material-ui/core/Input';
-import Icon from '@material-ui/core/Icon';
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
 
