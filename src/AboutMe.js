33d87e45a7157aaea93e3eb6827730e412b51d1a
Adam Brown <Adam.Brown2@coxautoinc.com>
2020-07-03 20:59:00 -0400

    updates content

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -18,7 +18,7 @@ const AboutMe = () => {
 
                     <p>I am currently employed with Cox Automotive working with Autotrader on their Marketplace Delivery Stream / Search Results Page.  We strive to make highly-efficient components for Autotrader.com using React.js and the latest tools following Agile methodologies.</p>
 
-                    <p>My background is anything but traditional, although I did manage to complete a Bachelor's of Science in Mathematics from UGA somehow.  I have always been into programming, but didn't officially pursue the career until 2018.</p>
+                    <p>My background is anything but traditional, although I did manage to complete a Bachelor's of Science in Mathematics from UGA somehow.  I have always been into programming, but didn't officially pursue the career until 2018 outside of a few contract jobs.</p>
                 </div>
             </div>
             <HowCanIHelp />
