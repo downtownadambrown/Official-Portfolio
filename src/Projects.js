bc91a742d64826a7adeef226f6f2eca99676a880
Adam Brown <adam.brown2@coxautoinc.com>
2020-09-17 15:57:11 -0400

    builds HoverableDisplay component

--- src/Projects.js
+++ src/Projects.js
@@ -3,8 +3,10 @@ import './Projects.css';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import portfolioProject from './static/portfolio-project.png';
+import bamazonProject from './static/bamazon-project.png';
 import dhsComponent from './static/dhs-component.png';
 import packagesComponent from './static/packages-component.png';
+import HoverableDisplay from "./HoverableDisplay";
 
 const TabPanel = (props) => {
     const { children, value, index, ...other } = props;
@@ -20,29 +22,28 @@ const TabPanel = (props) => {
     );
 };
 
+
 const AppProjects = () => {
     return (
-        <div className="container d-flex flex-wrap align-self-center justify-self-center h-100">
-            <div className="col-lg-4 col-md-6 col-sm-12 mb-4" style={{ height: 200 }}>
-                <div className="bg-dark d-flex justify-content-center" style={{ width: "100%", height: "100%", borderRadius: 15 }}>
-                    <img src={portfolioProject} style={{ height: '100%' }} alt="Adam's Portfolio Project" />
-                </div>
-            </div>
-            <div className="col-lg-4 col-md-6 col-sm-12 mb-4" style={{ height: 200 }}>
-                <div className="bg-dark" style={{ width: "100%", height: "100%", borderRadius: 15 }}>
-                    <span>Jobster</span>
-                </div>
-            </div>
-            {/*<div className="col-lg-4 col-md-6 col-sm-12 mb-4" style={{ height: 200 }}>
-                <div className="bg-dark" style={{ width: "100%", height: "100%", borderRadius: 15 }}>
-
-                </div>
-            </div>
-            <div className="col-lg-4 col-md-6 col-sm-12 mb-4" style={{ height: 200 }}>
-                <div className="bg-dark" style={{ width: "100%", height: "100%", borderRadius: 15 }}>
-
-                </div>
-            </div>*/}
+        <div className="container d-flex flex-wrap h-100">
+            <HoverableDisplay
+                className="col-lg-6 col-md-12 appTile d-flex justify-content-center align-items-center"
+                title="My Website (this site)"
+                subTitle="Built to help Engineers, a lot of love and experimentation went into this app :)"
+                technologies={['React', 'React-Spring', 'Material-UI', 'Hooks']}
+                ctaText="View More +"
+                handleCTAClick={() => {}}
+                backgroundURL={portfolioProject}
+            />
+            <HoverableDisplay
+                className="col-lg-6 col-md-12 appTile d-flex justify-content-center align-items-center"
+                title="Bamazon"
+                subTitle="An Amazon-esque full-stack warehouse management app"
+                technologies={['ES6 Javascript', 'Node.js', 'Express', 'PostgreSQL', 'Sequelize', 'HTML5', 'CSS3', 'Bootstrap']}
+                ctaText="View More +"
+                handleCTAClick={() => {}}
+                backgroundURL={bamazonProject}
+            />
         </div>
     );
 };
@@ -50,7 +51,25 @@ const AppProjects = () => {
 const UIComponents = () => {
     return (
         <div className="container d-flex flex-wrap align-self-center justify-self-center h-100">
-            <div className="col-lg-4 col-md-6 col-sm-12 mb-4" style={{ height: 200 }}>
+            <HoverableDisplay
+                className="col-lg-6 col-md-12 appTile d-flex justify-content-center align-items-center"
+                title="Autotrader.com Feature"
+                subTitle="Dealer Home Services"
+                technologies={['React', 'Redux', 'Node', 'Express', 'Java/Spring', 'React-Bootstrap']}
+                ctaText="View More +"
+                handleCTAClick={() => {}}
+                backgroundURL={dhsComponent}
+            />
+            <HoverableDisplay
+                className="col-lg-6 col-md-12 appTile d-flex justify-content-center align-items-center"
+                title="Autotrader.com Feature"
+                subTitle="Vehicle Packages on Listing Card"
+                technologies={['React', 'Redux', 'Node', 'Express', 'React-Bootstrap']}
+                ctaText="View More +"
+                handleCTAClick={() => {}}
+                backgroundURL={packagesComponent}
+            />
+{/*            <div className="col-lg-4 col-md-6 col-sm-12 mb-4" style={{ height: 200 }}>
                 <div className="bg-dark d-flex justify-content-center" style={{ width: "100%", height: "100%", borderRadius: 15 }}>
                     <img src={dhsComponent} style={{ height: '100%' }} alt="Dealer Home Services UI Component" />
                 </div>
@@ -58,16 +77,6 @@ const UIComponents = () => {
             <div className="col-lg-4 col-md-6 col-sm-12 mb-4" style={{ height: 200 }}>
                 <div className="bg-dark fill" style={{ width: "100%", height: "100%", borderRadius: 15 }}>
                     <img src={packagesComponent} className="scale-down" alt="Dealer Home Services UI Component" />
-                </div>
-            </div>
-{/*            <div className="col-lg-4 col-md-6 col-sm-12 mb-4" style={{ height: 200 }}>
-                <div className="bg-dark" style={{ width: "100%", height: "100%", borderRadius: 15 }}>
-
-                </div>
-            </div>
-            <div className="col-lg-4 col-md-6 col-sm-12 mb-4" style={{ height: 200 }}>
-                <div className="bg-dark" style={{ width: "100%", height: "100%", borderRadius: 15 }}>
-
                 </div>
             </div>*/}
         </div>
@@ -119,37 +128,3 @@ const Projects = () => {
 };
 
 export default Projects;
-
-
-/*
-*         <div className="container d-flex align-self-center justify-self-center">
-            <div className="d-flex flex-grow-1">
-                <div className="col-2 p-0">
-                    <img src={portfolioProject} width={185} alt="Adam Brown Portfolio Project" />
-                    <div className="bottom-centered-strip text-secondary">
-                        My Portfolio (this site)
-                    </div>
-                </div>
-                <div className="d-flex flex-column col-10 p-0">
-                    <div className="d-flex flex-column">
-                        <div className="d-flex flex-row">
-                            <div className="col-2 font-weight-bold">Technologies:</div>
-                            <div className="col-10">React.js, ES6 Javascript, Memoization, Hooks, SASS, React-Spring, Material UI/Bootstrap</div>
-                        </div>
-                        <div className="d-flex flex-row mt-2">
-                            <div className="col-2 font-weight-bold">Concepts:</div>
-                            <div className="col-10">Memoization, Hooks (useState, useEffect, useCallback), Hoisting, Controlled Inputs, Text Masking</div>
-                        </div>
-                        <div className="d-flex flex-row mt-2">
-                            <div className="col-2 font-weight-bold">Description:</div>
-                            <div className="col-10">
-                                <span>
-                                    My goal of building this site was to make it as informative as possible also while creating a unique portfolio experience to help showcase my React development abilities and inspire others.  For this, I chose to use React-Spring for some fun transitions, wanted to leverage as much of Bootstrap as possible for simplicity, but also wanted to get things up and going in a reasonable timeframe which is why I chose to use Material UI for basic components.
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-* */
