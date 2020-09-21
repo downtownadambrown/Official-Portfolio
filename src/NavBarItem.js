9aa1a494786e9e2b61a6a8d801d253834fa596cd
Adam Brown <adam.brown2@coxautoinc.com>
2020-09-21 18:27:13 -0400

    sets bold on selected nav item

--- src/NavBarItem.js
+++ src/NavBarItem.js
@@ -5,14 +5,17 @@ import { useLocation, useHistory } from "react-router-dom";
 const NavBarItem = ({ label, route, first, altroute }) => {
     const history = useHistory();
     const location = useLocation();
+    const selected = location.pathname.includes(route) || location.pathname.includes(altroute);
+    const unselected = !location.pathname.includes(route) && !location.pathname.includes(altroute);
+
     const className = classnames({
-        "underline-from-left": !location.pathname.includes(route) && !location.pathname.includes(altroute),
-        "navbar-item-selected": location.pathname.includes(route) || location.pathname.includes(altroute),
+        "underline-from-left": unselected,
+        "navbar-item-selected": selected,
     }, "navbar-border navbar-item");
 
     return (
-        <div className={classnames({ "ml-2": !first }, "navbar-item-container centered")} onClick={() => history.push(route)}>
-            <span className="navbar-item centered">
+        <div className={classnames({ "ml-2": !first }, "navbar-item-container centered nav__item")} onClick={() => history.push(route)}>
+            <span className={classnames({ "font-weight-bold": selected }, "navbar-item nav__link centered")}>
                 {label}
             </span>
             <div className={className} />
