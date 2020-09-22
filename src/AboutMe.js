fa3793c49ed4ad9630390c955325740f8c95e8b6
Adam Brown <adam.brown2@coxautoinc.com>
2020-09-22 17:22:23 -0400

    cleans up css

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,7 +4,7 @@ import HowCanIHelp from './HowCanIHelp';
 
 const AboutMe = () => {
     return (
-        <div className="container d-flex flex-column align-items-center h-100 text-secondary">
+        <div className="container d-flex flex-column align-items-center text-secondary">
             <h2 className="font-weight-bold show-only-mobile">About Me</h2>
             <div className="container d-flex align-items-center flex-column flex-lg-row w-100 px-0">
                 <img
